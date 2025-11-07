3338caf7b1c0ece596609492ffc08ace12f9b2ba
pk370 <pk999178@gmail.comm>
2025-11-07 07:11:11 +0530

    local css done

--- 14-worldwise/src/App.jsx
+++ 14-worldwise/src/App.jsx
@@ -1,9 +1,10 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product";
+import Product from "././pages/Product";
 import Pricing from "././pages/Pricing";
 import HomePage from "././pages/HomePage";
 import PageNotFound from "././pages/PageNotFound";
+import AppLayout from "././pages/AppLayout";
 
 function App() {
   return <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
      <Route path="product" element={<Product/>} />
      <Route path="pricing" element={<Pricing/>} />
      <Route path="/" element={<HomePage/>} />
+     <Route path="app" element={<AppLayout />} />
      <Route path="*" element={<PageNotFound />} />
     </Routes>
   </BrowserRouter>
--- 14-worldwise/src/pages/HomePage.jsx
+++ 14-worldwise/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom"
 import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
 
 function HomePage() {
   return (
     <div>
         <PageNav/>
-      worldwise
+        <AppNav/>
+      <h1>worldwise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   )
 }
