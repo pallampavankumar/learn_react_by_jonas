1b0bc53b1094d5917871d67724179e34206de790
pk370 <pk999178@gmail.comm>
2025-11-07 08:21:03 +0530

    210-video

--- 14-worldwise/src/App.jsx
+++ 14-worldwise/src/App.jsx
@@ -5,15 +5,16 @@ import Pricing from "././pages/Pricing";
 import HomePage from "././pages/HomePage";
 import PageNotFound from "././pages/PageNotFound";
 import AppLayout from "././pages/AppLayout";
-
+import Login from "./pages/Login";
 function App() {
   return <BrowserRouter>
     <Routes>
      <Route path="product" element={<Product/>} />
      <Route path="pricing" element={<Pricing/>} />
-     <Route path="/" element={<HomePage/>} />
      <Route path="app" element={<AppLayout />} />
+     <Route path="/" element={<HomePage/>} />
      <Route path="*" element={<PageNotFound />} />
+     <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
 }
